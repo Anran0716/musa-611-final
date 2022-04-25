8b0105257f6e850a61c83b305fa80e7ea31f4031
Anran0716 <anranzheng0716@163.com>
2022-04-25 12:51:39 -0400

    revise route

--- js/FindBestRouteScript.js
+++ js/FindBestRouteScript.js
@@ -2,6 +2,11 @@ var map = L.map('map', {
   center: [40.343182, -105.688103],
   zoom: 11
 });
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoieGludGlhbiIsImEiOiJjazh1bGtkOXMwY2h4M25wYXh2d3J5NGpzIn0.6f78lOG9zSD3Iicqt6nXqQ', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -20,3 +25,14 @@ var queryString = window.location.search; // Returns:'?orig=YMCA+of+the+Rockies&
 let params = new URLSearchParams(queryString);
 let originName = params.get("orig"); // is the number 'YMCA of the Rockies'
 let destinatioNname = params.get("dest"); // is the number 'Wind River Bluff'
+
+//according to the input origin and destination, find ou
+//test with one dataset
+//switch the route name into desination 
+console.log(`The current origin is ${originName}.`);
+let url = `https://raw.githubusercontent.com/Anran0716/musa-611-final/main/rawdata/Tour route/${originName}.geojson`;
+fetch(url)
+   .then(resp => resp.json())
+   .then(poi_data => {
+     const geoJsonLayer1 = L.geoJSON(poi_data, {style: myStyle})
+})
