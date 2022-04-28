935696a27afda7afaeac6fa023b800b92e15e28d
Anran0716 <anranzheng0716@163.com>
2022-04-28 15:09:53 -0400

    revise data

--- js/FindBestRouteScript.js
+++ js/FindBestRouteScript.js
@@ -2,11 +2,14 @@ var map = L.map('map', {
   center: [40.343182, -105.688103],
   zoom: 11
 });
-var myStyle = {
-    "color": "#ff7800",
-    "weight": 5,
-    "opacity": 0.65
+var geojsonLineOptions = {
+  weight: 3,
+  color: "#fa7f88",
+  fillOpacity: 0
 };
+
+const partySelect = document.querySelector('#party-filter');
+const partySelect1 = document.querySelector('#party-filter1');
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoieGludGlhbiIsImEiOiJjazh1bGtkOXMwY2h4M25wYXh2d3J5NGpzIn0.6f78lOG9zSD3Iicqt6nXqQ', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -28,11 +31,21 @@ let destinatioNname = params.get("dest"); // is the number 'Wind River Bluff'
 
 //according to the input origin and destination, find ou
 //test with one dataset
-//switch the route name into desination 
+//switch the route name into desination
 console.log(`The current origin is ${originName}.`);
 let url = `https://raw.githubusercontent.com/Anran0716/musa-611-final/main/rawdata/Tour route/${originName}.geojson`;
 fetch(url)
    .then(resp => resp.json())
    .then(poi_data => {
-     const geoJsonLayer1 = L.geoJSON(poi_data, {style: myStyle})
+     let feature1;
+     for (let i=0;i<poi_data.features.length;i++){
+       const f = poi_data.features[i];
+       if(f.properties.name == destinatioNname)
+       {
+         let feature1=f;
+         //insert origin & destination
+
+        const geoJsonLayer1 =  L.geoJSON(feature1, {style: geojsonLineOptions}).addTo(layerGroup);
+       }
+     }
 })
